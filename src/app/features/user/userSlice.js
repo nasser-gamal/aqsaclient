f14540bf488dbc89898c07356a13923d86225672
Nasser Gamal <98104328+nasser-gamal@users.noreply.github.com>
2023-07-09 21:28:44 +0300

    g

--- src/app/features/user/userSlice.js
+++ src/app/features/user/userSlice.js
@@ -3,7 +3,7 @@ import Cookies from 'js-cookie';
 
 const initialState = {
   user: Cookies.get('user') ? JSON.parse(Cookies.get('user')) : null,
-  token: Cookies.get('token') ? JSON.parse(Cookies.get('token')) : null,
+ 
 };
 
 export const userSlice = createSlice({
@@ -12,16 +12,13 @@ export const userSlice = createSlice({
   reducers: {
     setCredentials: (state, action) => {
       state.user = action.payload.user;
-      state.token = action.payload.token;
       Cookies.set('user', JSON.stringify(action.payload.user));
-      Cookies.set('token', JSON.stringify(action.payload.token));
+      
     },
 
     logOut: (state) => {
       state.user = null;
-      state.token = null;
       Cookies.set('user', null);
-      Cookies.set('token', null);
     },
   },
 });
