a19050ca1ea609b7aa55a2c30697fac8332ea637
Nasser Gamal <98104328+nasser-gamal@users.noreply.github.com>
2023-07-10 22:28:14 +0300

    G

--- src/utils/formatDate.js
+++ src/utils/formatDate.js
@@ -10,11 +10,12 @@ export const formattedDate = (dateValue) => {
 
 export const formattedTime = (value) => {
   const dateTime = new Date(value);
-  const hours = dateTime.getHours();
-  const minutes = dateTime.getMinutes();
-  const amPm = hours < 12 ? 'ص' : 'م';
+  const hours = dateTime.getUTCHours();
+  const minutes = dateTime.getUTCMinutes();
+  const amPm = hours < 12 ? 'AM' : 'PM';
   const formattedHours = (hours % 12) || 12;
   const formattedTime = `${formattedHours}:${minutes.toString().padStart(2, '0')} ${amPm}`;
   return formattedTime;
 };
+
 
