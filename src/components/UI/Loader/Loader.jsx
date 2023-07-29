f8e79d484cade20904bea8a9b6b8e9cb6fa50393
Nasser Gamal <98104328+nasser-gamal@users.noreply.github.com>
2023-07-29 13:45:03 +0300

    g

--- src/components/UI/Loader/Loader.jsx
+++ src/components/UI/Loader/Loader.jsx
@@ -1,13 +1,15 @@
 
 import Portal from '../../../utils/Portal';
 import './loader.modules.css';
-
+import loader from '../../../assets/load.gif'
 
 export default function Loader() {
   return (
     <Portal>
       <div className="loader">
-        <div className="loader-inner"></div>
+        <div>
+          <img src={loader} alt={loader} />
+        </div>
       </div>
     </Portal>
   )
