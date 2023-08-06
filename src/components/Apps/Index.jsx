87f72935b5566951235ad2d571e49ec81f53bf2b
Nasser Gamal <98104328+nasser-gamal@users.noreply.github.com>
2023-08-06 08:39:18 +0300

    G

--- src/components/Apps/Index.jsx
+++ src/components/Apps/Index.jsx
@@ -1,10 +1,23 @@
+import { useEffect } from 'react';
 import { useFindAllAppsQuery } from '../../app/features/applications/applicationsApi';
 import AddButton from '../common/Button/AddButton';
 import AppTable from './Table/AppTable';
+import { useDispatch } from 'react-redux';
+import { hideLoader, showLoader } from '../../app/features/loader/loaderSlice';
 
 
 export default function Index() {
+  const dispatch = useDispatch();
   const { data, isLoading } = useFindAllAppsQuery();
+
+
+  useEffect(() => {
+    if (isLoading) {
+      dispatch(showLoader());
+    } else {
+      dispatch(hideLoader());
+    }
+  }, [dispatch, isLoading])
 
   return (
     <>
